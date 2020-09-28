64a4f1f095215a4c5c81212db925c91ca31c5d4f
EC2 Default User <ec2-user@ip-172-31-11-248.ap-northeast-1.compute.internal>
2020-09-28 08:12:34 +0000

    installed rubocop pre-commit

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,6 +59,6 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
-  
-  config.hosts << "5c491fbfd4c0495c85c4a6743957011a.vfs.cloud9.ap-northeast-1.amazonaws.com"
+
+  config.hosts << '5c491fbfd4c0495c85c4a6743957011a.vfs.cloud9.ap-northeast-1.amazonaws.com'
 end
