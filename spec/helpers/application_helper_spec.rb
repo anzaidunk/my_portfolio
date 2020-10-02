e5d39e70c1d1231e1e060cc1fd83b9b05ac56994
anzaidunk <hrm4895@gmail.com>
2020-10-02 02:55:21 +0000

    forget test

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -11,16 +11,15 @@ require 'rails_helper'
 #   end
 # end
 RSpec.describe ApplicationHelper, type: :helper do
-  
-  describe "#full_title" do
-    context "page_title is empty" do
-      it "removes symbol" do
+  describe '#full_title' do
+    context 'page_title is empty' do
+      it 'removes symbol' do
         expect(helper.full_title).to eq('Move')
       end
     end
 
-    context "page_title is not empty" do
-      it "returns title and application name where contains symbol" do
+    context 'page_title is not empty' do
+      it 'returns title and application name where contains symbol' do
         expect(helper.full_title('hoge')).to eq('hoge | Move')
       end
     end
